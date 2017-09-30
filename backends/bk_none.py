0119792dfe5e145cc2a5e48f643e98fd9d10098f
Zhang Huangbin <zhb@iredmail.org>
2017-09-30 19:54:32 +0800

    Add missing function in backends/bk_none.py. Fix incorrect return value in backends/bk_none.py.

--- backends/bk_none.py
+++ backends/bk_none.py
@@ -1,21 +1,24 @@
 # Do nothing to interactive with SQL/LDAP/... backends.
 
 
-def is_domain_exists(domain, conn=None):
+def is_domain_exists(domain, *args, **kw):
     return True
 
 
-def is_email_exists(mail, conn=None):
+def is_email_exists(mail, *args, **kw):
     return False
 
 
-def is_maillist_exists(mail, conn=None):
-    return False
+def is_maillist_exists(mail, *args, **kw):
+    return True
 
 
-def add_maillist(mail, conn=None):
+def add_maillist(mail, *args, **kw):
     return (True, )
 
 
-def remove_maillist(mail, conn=None):
+def update_maillist(mail, *args, **kw):
+    return (True, )
+
+def remove_maillist(mail, *args, **kw):
     return (True, )
