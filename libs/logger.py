1acb0ff38052066d702066295f0299f1aa0b3b34
Zhang Huangbin <zhb@iredmail.org>
2019-03-27 22:12:53 +0800

    Code cleanup.

--- libs/logger.py
+++ libs/logger.py
@@ -9,18 +9,16 @@ logger = logging.getLogger('mlmmjadmin')
 _log_level = getattr(logging, str(settings.log_level).upper())
 logger.setLevel(_log_level)
 
-# Syslog handler
-if settings.SYSLOG_SERVER.startswith('/'):
-    # Log to a local socket
-    _handler = SysLogHandler(address=settings.SYSLOG_SERVER,
-                             facility=settings.SYSLOG_FACILITY)
-else:
-    # Log to a network address
-    _handler = SysLogHandler(address=(settings.SYSLOG_SERVER, settings.SYSLOG_PORT),
-                             facility=settings.SYSLOG_FACILITY)
-
 # Log format
 _formatter = logging.Formatter('%(name)s %(levelname)s %(message)s')
 
+if settings.SYSLOG_SERVER.startswith('/'):
+    # Log to a local socket
+    _server = settings.SYSLOG_SERVER
+else:
+    # Log to a network address
+    _server = (settings.SYSLOG_SERVER, settings.SYSLOG_PORT)
+
+_handler = SysLogHandler(address=_server, facility=settings.SYSLOG_FACILITY)
 _handler.setFormatter(_formatter)
 logger.addHandler(_handler)
