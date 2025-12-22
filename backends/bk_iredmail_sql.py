20f291aee06c2bd12357bfa1b725371946344c79
ly020044 <ly020044@outlook.com>
2025-12-22 19:28:23 +0800

    Add SSL support to database connection

--- backends/bk_iredmail_sql.py
+++ backends/bk_iredmail_sql.py
@@ -40,6 +40,7 @@ class MYSQLWrap(object):
         conn = web.database(dbn='mysql',
                             host=settings.iredmail_sql_db_server,
                             port=int(settings.iredmail_sql_db_port),
+                            ssl={'ssl': settings.iredmail_sql_use_ssl},
                             db=settings.iredmail_sql_db_name,
                             user=settings.iredmail_sql_db_user,
                             pw=settings.iredmail_sql_db_password,
